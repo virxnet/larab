05499981df91be3c66d848623db3d5316bd35ab6
Antony Shan Peiris <49996735+antonypeiris@users.noreply.github.com>
2023-03-15 15:25:10 -0700

    Update 2022_01_14_100002_alter_users_table_add_api_fields.php
    fixing typo

--- database/migrations/2022_01_14_100002_alter_users_table_add_api_fields.php
+++ database/migrations/2022_01_14_100002_alter_users_table_add_api_fields.php
@@ -15,10 +15,10 @@ class AlterUsersTableAddApiFields extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('uid')->nullable()->after('id');
-            if (!Schema::hasColumn('users', 'first_name') {
+            if (!Schema::hasColumn('users', 'first_name')) {
                 $table->string('first_name')->nullable();
             }
-            if (!Schema::hasColumn('users', 'last_name') {
+            if (!Schema::hasColumn('users', 'last_name')) {
                 $table->string('last_name')->nullable();
             }
             $table->boolean('is_active')->default(true);
